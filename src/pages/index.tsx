bd40bcb22462ef730f984adf701463b12de2db46
shuai.yang <shuai.yang@highlands.ltd>
2022-05-11 17:36:46 +0800

    尝试提交

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
+/*
+ * @Author: shuai.yang shuai.yang@highlands.ltd
+ * @Date: 2022-05-11 16:52:47
+ * @LastEditors: shuai.yang shuai.yang@highlands.ltd
+ * @LastEditTime: 2022-05-11 17:36:27
+ * @FilePath: \testForReact\src\pages\index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import styles from './index.less';
 
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>Page index1</h1>
     </div>
   );
 }
